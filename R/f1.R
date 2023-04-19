5e73f4186b1e4edb57e64cea6e2cba15a64573a0
Luisagi <ariasgiraldo.luisfelipe@gmail.com>
2023-04-19 16:54:48 -0500

    v 0.0.1 documentation f1

--- R/f1.R
+++ R/f1.R
@@ -1,5 +1,13 @@
-f <- function(x){
-  return(x^2)
+#' Root Square
+#'
+#' @param x (numeric)
+#'
+#' @return the root square of a number.
+#' @export
+#'
+#' @examples
+#' rootsquare <- f1(9)
+f1 <- function(x){
+  return(sqrt(x))
 }
 
-f(2)

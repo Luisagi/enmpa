2fd12c2f8eaa7bfd09cfa06b4231f719a94738c5
luisagi <ariasgiraldo.luisfelipe@gmail.com>
2023-08-04 14:08:58 +0200

    v0.0.7 add "niche signal functions" (Cobos & Peterson, 2021)

---
+++ R/niche_signal.R
@@ -0,0 +1,259 @@
+#' Niche Signal
+#'
+#' @description
+#' Detection of niche signal with records of positive a negative detection
+#' (\href{https://doi.org/10.17161/bi.v17i.15985}{Cobos & Peterson, 2022}).
+#'
+#' @usage
+#' niche_signal(data, condition, variables, method = "univariate",
+#'              permanova_method = "mahalanobis", iterations = 1000,
+#'              set_seed = 1, verbose = TRUE, ...)
+#'
+#' @param data matrix or data.frame containing at least the following
+#' information: a column representing \code{condition} (positive = 1 or
+#' negative = 0), and one or more columns representing environmental variables.
+#' @param condition (character) name of the column with numeric information
+#' about detection (positive = 1 or negative = 0).
+#' @param variables (character) vector of one or more names of columns to be
+#' used as environmental variables. If \code{method} = "univariate", only one
+#' variable is used; for \code{method} = "permanova", multiple variables can
+#' be used.
+#' @param method (character) name of the method to be used for niche comparison.
+#' Default = "univariate".
+#' @param permanova_method (character) name of the dissimilarity index to be
+#' used as \code{method} in \code{\link[vegan]{adonis2}}. See all options in
+#' \code{\link[vegan]{vegdist}}. Default = "mahalanobis".
+#' @param iterations (numeric) number of iterations to be used in analysis.
+#' Default = 1000. If \code{method} = "permanova", permutations = iterations - 1.
+#' @param set_seed (numeric) integer value to specify a initial seed.
+#' Default = 1.
+#' @param verbose (logical) whether or not to print messages about the process.
+#' Default = TRUE.
+#' @param ... other arguments to be passed to \code{\link[vegan]{adonis2}}.
+#'
+#' @rdname niche_signal
+#'
+#' @importFrom vegan adonis2
+#' @importFrom stats median sd ecdf
+
+niche_signal <- function(data, condition, variables, method = "univariate",
+                         permanova_method = "mahalanobis", iterations = 1000,
+                         set_seed = 1, verbose = TRUE, ...) {
+  # initial tests
+  if (!method %in% c("univariate", "permanova")) {
+    stop("Argument 'method' not valid, options are 'univariate' and 'permanova'")
+  }
+  if (missing(variables)) {
+    stop("Argument 'variables' must be defined")
+  }
+
+  # number of positives
+  n_pos <- nrow(data[data[, condition] == 1, ])
+
+  # running analysis
+  if (method == "univariate") {
+    if (length(variables) > 1) {
+      if (verbose) {
+        message("More than one element detected in 'variables', analysis",
+                "\nwill run with the first variable with method 'univariate'")
+      }
+
+      variables <- variables[1]
+    }
+
+    res <- niche_signal_univariate(data, condition, variables, iterations,
+                                   set_seed, verbose)
+
+  } else {
+    if (verbose) {
+      message("Number of permutations for permanova = interations - 1")
+    }
+
+    res <- niche_signal_permanova(data, condition, variables,
+                                  permutations = (iterations - 1),
+                                  permanova_method, verbose, ...)
+  }
+
+  # preparing final result
+  res <- list(
+    data = data, modified_data = res$modified_data,
+    summary = list(variables = variables, condition = condition,
+                   method = method, n_complete = nrow(data),
+                   n_positive = n_pos),
+    univariate_results = res$analysis_results,
+    permanova_results = res$permanova_results
+  )
+
+  # returning result
+  return(res)
+}
+
+
+#' @rdname niche_signal
+#' @param variable (character) name of the column containing data to be used
+#' as environmental variable.
+#' @usage
+#' niche_signal_univariate(data, condition, variable, iterations = 1000,
+#'                         set_seed = 1, verbose = TRUE)
+
+niche_signal_univariate <- function(data, condition, variable, iterations = 1000,
+                                    set_seed = 1, verbose = TRUE) {
+  # initial tests
+  if (missing(data)) {
+    stop("Argument 'data' must be defined")
+  }
+  if (missing(condition)) {
+    stop("Argument 'condition' must be defined")
+  }
+  if (missing(variable)) {
+    stop("Argument 'variable' must be defined")
+  }
+
+  # number of infected hosts
+  if (verbose) {
+    message("Preparing data")
+  }
+  n_inf <- nrow(data[data[, condition] == 1, ])
+
+  # representation of niches via sd and range
+  posit <- data[, condition] == 1
+  pa_mean <- mean(data[posit, variable])
+  pa_med <- median(data[posit, variable])
+  pa_sd <- sd(data[posit, variable])
+  pa_range <- diff(range(data[posit, variable]))
+
+  h_mean <- mean(data[, variable])
+  h_med <- median(data[, variable])
+  h_sd <- sd(data[, variable])
+  h_range <- diff(range(data[, variable]))
+
+  # positive cases null distribution of niches
+  if (verbose) {
+    message("Running analysis...")
+  }
+  ## mean
+  set.seed(set_seed)
+  n_mean <- vapply(1:iterations, FUN.VALUE = numeric(1), function(x) {
+    mean(sample(data[, variable], n_inf))
+  })
+
+  ## median
+  set.seed(set_seed)
+  n_med <- vapply(1:iterations, FUN.VALUE = numeric(1), function(x) {
+    median(sample(data[, variable], n_inf))
+  })
+
+  ## sd
+  set.seed(set_seed)
+  n_sd <- vapply(1:iterations, FUN.VALUE = numeric(1), function(x) {
+    sd(sample(data[, variable], n_inf))
+  })
+
+  ## range
+  set.seed(set_seed)
+  n_range <- vapply(1:iterations, FUN.VALUE = numeric(1), function(x) {
+    diff(range(sample(data[, variable], n_inf)))
+  })
+
+  # helper to calculated p value according to position of observed in null
+  p_calc <- function(observed, null_distribution) {
+    ef <- ecdf(null_distribution)
+    ef(observed)
+  }
+
+  # pseudo p values
+  pml <- p_calc(pa_mean, n_mean)
+  pmdl <- p_calc(pa_med, n_med)
+  psl <- p_calc(pa_sd, n_sd)
+  prl <- p_calc(pa_range, n_range)
+
+  # hypothesis test
+  tm <- ifelse(pml <= 0.025 | pml >= 0.975, "rejected", "accepted")
+  tmd <- ifelse(pmdl <= 0.025 | pmdl >= 0.975, "rejected", "accepted")
+  ts <- ifelse(psl <= 0.025 | psl >= 0.975, "rejected", "accepted")
+  tr <- ifelse(prl <= 0.025 | prl >= 0.975, "rejected", "accepted")
+
+  # position of observed
+  qm <- ifelse(tm == "accepted", "indistinct",
+               ifelse(pml <= 0.025, "lower", "higher"))
+  qmd <- ifelse(tmd == "accepted", "indistinct",
+                ifelse(pmdl <= 0.025, "lower", "higher"))
+  qs <- ifelse(ts == "accepted", "indistinct",
+               ifelse(psl <= 0.025, "lower", "higher"))
+  qr <- ifelse(tr == "accepted", "indistinct",
+               ifelse(prl <= 0.025, "lower", "higher"))
+
+
+  # return results
+  results <- list(
+    data = data,
+    summary = list(variable = variable, condition = condition,
+                   n_complete = nrow(data), n_positive = n_inf),
+    analysis_results = list(
+      hypothesis_test = c(h0_mean = tm, h0_median = tmd, h0_SD = ts,
+                          h0_range = tr, positive_mean_vs_null = qm,
+                          positive_median_vs_null = qmd,
+                          positive_SD_vs_null = qs,
+                          positive_range_vs_null = qr),
+      complete_niche = c(mean = h_mean, median = h_med, SD = h_sd,
+                         range = h_range),
+      positive_niche = c(mean = pa_mean, median = pa_med, SD = pa_sd,
+                         range = pa_range),
+      null_results = list(mean = n_mean, median = n_med, SD = n_sd,
+                          range = n_range)
+    )
+  )
+
+  return(results)
+}
+
+
+
+
+#' @rdname niche_signal
+#' @param permutations number of permutations to be performed.
+#' @usage
+#' niche_signal_permanova(data, condition, variables, permutations = 999,
+#'                        permanova_method = "mahalanobis", verbose = TRUE, ...)
+
+niche_signal_permanova <- function(data, condition, variables, permutations = 999,
+                                   permanova_method = "mahalanobis",
+                                   verbose = TRUE, ...) {
+  # initial tests
+  if (missing(data)) {
+    stop("Argument 'data' must be defined")
+  }
+  if (missing(condition)) {
+    stop("Argument 'condition' must be defined")
+  }
+  if (missing(variables)) {
+    stop("Argument 'variables' must be defined")
+  }
+
+  # preparing data
+  if (verbose) {
+    message("Preparing data")
+  }
+  n_com <- nrow(data)
+  dataihost <- data[data[, condition] == 1, ]
+  n_inf <- nrow(dataihost)
+  dataihost[, condition] <- 0
+  data <- rbind(data, dataihost)
+  form <- paste("as.matrix(data[, variables]) ~", condition)
+
+  # running analysis
+  if (verbose) {
+    message("Running analysis...")
+  }
+  perm <- vegan::adonis2(as.formula(form), data = data,
+                         permutations = permutations,
+                         method = permanova_method, ...)
+
+  # results preparation
+  results <- list(modified_data = data,
+                  summary = list(variables = variables, condition = condition,
+                                 n_complete = n_com, n_positive = n_inf),
+                  permanova_results = perm)
+
+  return(results)
+}
---
+++ R/plot_niche_signal.R
@@ -0,0 +1,308 @@
+#' Plot Niche Signal
+#'
+#' @description
+#' Plots to interpret results from niche_signal tests
+#' (\href{https://doi.org/10.17161/bi.v17i.15985}{Cobos & Peterson, 2022}).
+#'
+#' @usage
+#' plot_niche_signal(niche_signal_list, statistic = "mean",
+#'                   variables = NULL, ellipses = FALSE, level = 0.99,
+#'                   breaks = "Sturges", main = "", xlab = NULL, ylab = NULL,
+#'                   h_col = "lightgray", h_cex = 0.8, lty = 2, lwd = 1,
+#'                   l_col = c("blue", "black"), e_col = c("black", "red"),
+#'                   pch = 19, pt_cex = c(1.3, 0.8), pt_col = c("black", "red"),
+#'                   ...)
+#'
+#' @param niche_signal_list list of results from niche_signal.
+#' @param statistic (character) name of the statistic for which results will be
+#' explored when results come for univariate analysis. Default = "mean". Options
+#' are: "mean", "median", "SD", and "range".
+#' @param variables (character) name of variables to used in plots when
+#' results come from analysis using the `permanova` method. The default, NULL,
+#' uses the first two variables.
+#' @param ellipses (logical) whether to use ellipses to represent all and
+#' positive data when results come from PERMANOVA. The default, FALSE, plots
+#' points instead.
+#' @param level (numeric) value from 0 to 1 representing the limit of
+#' the ellipse to be plotted. Default = 0.99.
+#' @param main (character) title for plot. Default = "".
+#' @param xlab (character) x axis label. Default = NULL. For results from
+#' PERMANOVA, appropriate variable names are used.
+#' @param ylab (character) y axis label. Default = NULL. For univariate
+#' results, the default turn into "Frequency". For results from PERMANOVA,
+#' appropriate variable names are used.
+#' @param breaks breaks in the histogram as in \code{\link[graphics]{hist}}.
+#' Default = "Sturges".
+#' @param h_col a color to be used to fill the bars of histograms.
+#' Default = "lightgray".
+#' @param h_cex (numeric) value by which plotting text and symbols should be
+#' magnified relative to the default in histograms. Default = 0.8.
+#' @param lty (numeric) line type. See options in \code{\link[graphics]{par}}.
+#' Default = 2. A vector of length = 2 can be used, in order, all and positive.
+#' @param lwd (numeric) line width. See options in \code{\link[graphics]{par}}.
+#' Default = 1. A vector of length = 2 can be used, in order, all and positive.
+#' @param l_col line color for observed value of positives and confidence
+#' intervals. Default = c("blue", "black").
+#' @param e_col color of ellipse lines for all and positive data.
+#' Default = c("black", "red").
+#' @param pch point type. See options in \code{\link[graphics]{points}}.
+#' Default = 19. A vector of length = 2 can be used, in order, all and positive.
+#' @param pt_cex (numeric) value by which points will be magnified. Values
+#' for all and positive points are recommended. Default = c(1.3, 0.8).
+#' @param pt_col color for points. Values for all and positive points are
+#' recommended. Default = c("black", "red").
+#' @param ... other plotting arguments to be used.
+#'
+#' @rdname plot_niche_signal
+#' @export
+#'
+#' @importFrom ellipse ellipse
+#' @importFrom stats cov anova ecdf quantile
+#' @importFrom graphics lines box hist points
+
+
+plot_niche_signal <- function(niche_signal_list, statistic = "mean",
+                              variables = NULL, ellipses = FALSE,
+                              level = 0.99, breaks = "Sturges", main = "",
+                              xlab = NULL, ylab = NULL, h_col = "lightgray",
+                              h_cex = 0.8, lty = 2, lwd = 1,
+                              l_col = c("blue", "black"),
+                              e_col = c("black", "red"),
+                              pch = 19, pt_cex = c(1.3, 0.8),
+                              pt_col = c("black", "red"), ...) {
+
+  # method using to produce niche_signal_list
+  method <- niche_signal_list$summary$method
+
+  if (!is.null(method)) {
+    # plotting results from general analysis
+    if (method == "univariate") {
+      rlist <- list(
+        data = niche_signal_list$data,
+        summary = niche_signal_list$summary,
+        analysis_results = niche_signal_list$univariate_results
+      )
+
+      plot_niche_signal_univariate(rlist, statistic, breaks, main, xlab,
+                                   ylab, h_col, h_cex, lty, lwd, l_col, ...)
+
+    } else {
+      rlist <- list(
+        modified_data = niche_signal_list$modified_data,
+        summary = niche_signal_list$summary,
+        permanova_results = niche_signal_list$permanova_results
+      )
+
+      plot_niche_signal_permanova(rlist, variables,
+                                  ellipses, level, main, xlab, ylab, e_col,
+                                  lty, lwd, pch, pt_cex, pt_col, ...)
+    }
+  } else {
+    # plotting results from univariate analysis
+    if (!is.null(niche_signal_list$analysis_results)) {
+      plot_niche_signal_univariate(niche_signal_list, statistic,
+                                   breaks, main, xlab, ylab, h_col,
+                                   h_cex, lty, lwd, l_col, ...)
+    } else {
+      # plotting results from permanova results
+      if (!is.null(niche_signal_list$permanova_results)) {
+        plot_niche_signal_permanova(niche_signal_list, variables,
+                                    ellipses, level, main, xlab, ylab, e_col,
+                                    lty, lwd, pch, pt_cex, pt_col, ...)
+      }
+    }
+  }
+}
+
+
+
+#' @rdname plot_niche_signal
+#' @param niche_signal_univariate_list list of results from niche_signal_univariate.
+#' @usage
+#' plot_niche_signal_univariate(niche_signal_univariate_list, statistic = "mean",
+#'                              breaks = "Sturges", main = "", xlab = NULL,
+#'                              ylab = "Frequency", h_col = "lightgray",
+#'                              h_cex = 0.8, lty = 2, lwd = 1,
+#'                              l_col = c("blue", "black"), ...)
+
+plot_niche_signal_univariate <- function(niche_signal_univariate_list, statistic = "mean",
+                                         breaks = "Sturges", main = "", xlab = NULL,
+                                         ylab = "Frequency", h_col = "lightgray",
+                                         h_cex = 0.8, lty = 2, lwd = 1,
+                                         l_col = c("blue", "black"), ...) {
+  # test
+  if (missing(niche_signal_univariate_list)) {
+    stop("Argument 'niche_signal_univariate_list' is missing")
+  }
+
+  # other graphical settings
+  if (length(l_col) == 1) {
+    l_col <- rep(l_col, 2)
+  } else {
+    l_col <- l_col[1:2]
+  }
+
+  if (length(lty) == 1) {
+    lty <- rep(lty, 2)
+  } else {
+    lty <- lty[1:2]
+  }
+
+  if (length(lwd) == 1) {
+    lwd <- rep(lwd, 2)
+  } else {
+    lwd <- lwd[1:2]
+  }
+
+  # preparing data
+  if (statistic == "mean") {
+    all <- niche_signal_univariate_list$analysis_results$null_results$mean
+    pos <- niche_signal_univariate_list$analysis_results$positive_niche["mean"]
+    ci <- quantile(niche_signal_univariate_list$analysis_results$null_results$mean,
+                   c(0.025, 0.975))
+  }
+
+  if (statistic == "median") {
+    all <- niche_signal_univariate_list$analysis_results$null_results$median
+    pos <- niche_signal_univariate_list$analysis_results$positive_niche["median"]
+    ci <- quantile(niche_signal_univariate_list$analysis_results$null_results$median,
+                   c(0.025, 0.975))
+  }
+
+  if (statistic == "SD") {
+    all <- niche_signal_univariate_list$analysis_results$null_results$SD
+    pos <- niche_signal_univariate_list$analysis_results$positive_niche["SD"]
+    ci <- quantile(niche_signal_univariate_list$analysis_results$null_results$SD,
+                   c(0.025, 0.975))
+  }
+
+  if (statistic == "range") {
+    all <- niche_signal_univariate_list$analysis_results$null_results$range
+    pos <- niche_signal_univariate_list$analysis_results$positive_niche["range"]
+    ci <- quantile(niche_signal_univariate_list$analysis_results$null_results$range,
+                   c(0.025, 0.975))
+  }
+
+  var <- niche_signal_univariate_list$summary$variable
+
+  # other things for plots
+  ylab <- ifelse(is.null(ylab), "Frequency", ylab)
+  xlab <- ifelse(is.null(xlab), paste0(var, " (", statistic, ")"), xlab)
+  xrang <- range(c(all, pos))
+
+  # plot
+  hist(all, breaks = breaks, xlim = xrang, main = main, xlab = xlab,
+       ylab = ylab, col = h_col, cex = h_cex, ...)
+  abline(v = pos, col = l_col[1], lty = lty[1], lwd = lty[1])
+  abline(v = ci, col = l_col[2], lty = lty[2], lwd = lty[2])
+  box(bty = "l")
+}
+
+
+
+#' @rdname plot_niche_signal
+#' @param niche_signal_permanova_list list of results from niche_signal_permanova.
+#' @usage
+#' plot_niche_signal_permanova(niche_signal_permanova_list, variables = NULL,
+#'                            ellipses = FALSE, level = 0.99, main = "",
+#'                            xlab = NULL, ylab = NULL,
+#'                            e_col = c("black", "red"), lty = 2, lwd = 1,
+#'                            pch = 19, pt_cex = c(1.3, 0.8),
+#'                            pt_col = c("black", "red"), ...)
+
+plot_niche_signal_permanova <- function(niche_signal_permanova_list, variables = NULL,
+                                        ellipses = FALSE, level = 0.99,
+                                        main = "", xlab = NULL, ylab = NULL,
+                                        e_col = c("black", "red"), lty = 2, lwd = 1,
+                                        pch = 19, pt_cex = c(1.3, 0.8),
+                                        pt_col = c("black", "red"), ...) {
+
+  # test
+  if (missing(niche_signal_permanova_list)) {
+    stop("Argument 'niche_signal_permanova_list' is missing")
+  }
+
+  # selecting variables to use
+  if (is.null(variables)) {
+    variables <- niche_signal_permanova_list$summary$variables[1:2]
+  } else {
+    variables <- variables[1:2]
+  }
+
+  # relevant data to plot
+  condition <- niche_signal_permanova_list$summary$condition
+  condition <- niche_signal_permanova_list$modified_data[, condition]
+
+  all <- condition == 0
+  posi <- condition == 1
+
+  # type of points
+  if (ellipses == FALSE) {
+    if (length(pch) == 1) {
+      pch <- c(pch, pch)
+    } else {
+      pch <- pch[1:2]
+    }
+
+    if (length(pt_col) == 1) {
+      pt_col <- c(pt_col, pt_col)
+    } else {
+      pt_col <- pt_col[1:2]
+    }
+
+    # labels
+    xlab <- ifelse(is.null(xlab), variables[1], xlab)
+    ylab <- ifelse(is.null(ylab), variables[2], ylab)
+
+    # plot
+    allp <- niche_signal_permanova_list$modified_data[all, variables]
+    plot(allp, main = main, xlab = xlab, ylab = ylab, pch = pch[1],
+         cex = pt_cex[1], col = pt_col[1], ...)
+
+    allp <- niche_signal_permanova_list$modified_data[posi, variables]
+    points(allp,
+           pch = pch[2], cex = pt_cex[2], col = pt_col[2])
+
+  } else {
+    # type of lines
+    if (length(e_col) == 1) {
+      e_col <- rep(e_col, 2)
+    } else {
+      e_col <- e_col[1:2]
+    }
+
+    if (length(lty) == 1) {
+      lty <- rep(lty, 2)
+    } else {
+      lty <- lty[1:2]
+    }
+
+    if (length(lwd) == 1) {
+      lwd <- rep(lwd, 2)
+    } else {
+      lwd <- lwd[1:2]
+    }
+
+    # all ellipse
+    hell <- ellipse::ellipse(
+      x = cov(niche_signal_permanova_list$modified_data[all, variables]),
+      centre = colMeans(niche_signal_permanova_list$modified_data[all, variables]),
+      level = level
+    )
+
+    # positive ellipse
+    pell <- ellipse::ellipse(
+      x = cov(niche_signal_permanova_list$modified_data[posi, variables]),
+      centre = colMeans(niche_signal_permanova_list$modified_data[posi, variables]),
+      level = level
+    )
+
+    # plot limits
+    rans <- apply(rbind(hell, pell), 2, range)
+
+    # plot
+    plot(rans, type = "n", main = main, xlab = xlab, ylab = ylab, ...)
+    lines(hell, col = e_col[1], lwd = lwd[1], lty = lty[1])
+    lines(pell, col = e_col[2], lwd = lwd[2], lty = lty[2])
+  }
+}

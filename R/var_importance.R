d703dcd11c63600466cb004eac07b3e0de40d089
luisagi <ariasgiraldo.luisfelipe@gmail.com>
2023-08-30 13:32:00 +0200

    v0.0.9 now the function allows to plot

--- R/var_importance.R
+++ R/var_importance.R
@@ -5,9 +5,12 @@
 #' based on explained deviance.
 #'
 #' @usage
-#' var_importance(model)
+#' var_importance(x, plot = TRUE)
 #'
-#' @param model an object of class "glm" which inherits from the class "lm".
+#' @param x an object of class "glm" or a "list" of them which inherit
+#' from the class "lm".
+#'
+#' @param plot `logical`, whether plot the variable contribution. Default = TRUE.
 #'
 #' @return  data.frame
 #'
@@ -16,36 +19,57 @@
 #'
 #'
 #' @importFrom stats update as.formula deviance coef
+#' @importFrom graphics barplot boxplot text
 
 
-var_importance <- function(model){
+var_importance <- function(x, plot = TRUE){
 
   # initial tests
-  if (missing(model)) {
+  if (missing(x)) {
     stop("Argument 'model' must be defined.")
   }
 
-  # deviance of the full model
-  dev_full <- deviance(model)
+  if (check_if_glm_list(x)){
 
-  # deviance of the reduced models
-  dev_reduction <- sapply(names(coef(model))[-1], function(x) {
-    dev_full - get_red_dev(model, x)
-  })
+    aux <- lapply(x, function(y){var_importance_ind(y)})
+    tab_contr <- do.call(rbind, aux)[,1:2]
 
-  deviance_importance <- dev_reduction / sum(dev_reduction)
+    tab_contr$Models <- rep(names(aux), times = sapply(aux, nrow))
+    rownames(tab_contr) <- NULL
 
-  # preparing results
-  tab_contr <- data.frame(predictor = names(deviance_importance),
-                          stringsAsFactors = FALSE)
-  tab_contr$contribution <- deviance_importance
+    if (plot){
+      # Create a boxplot
+      boxplot(contribution ~ predictor, data = tab_contr,
+              xlab = NULL, ylab = "Relative contribution",
+              main = "Variable importance",
+              ylim = c(0, max(tab_contr$contribution) + 0.2)
+              )
+      #Calculate means and counts per group
+      means  <- tapply(tab_contr$contribution, tab_contr$predictor, mean)
+      counts <- tapply(tab_contr$contribution, tab_contr$predictor, length)
 
-  ord <- order(tab_contr$contribution, decreasing = TRUE)
-  tab_contr <- tab_contr[ord,]
-  tab_contr$cum_contribution <- cumsum(tab_contr$contribution)
+      # Common y coordinate for text labels in the upper part of the plot
+      text_y <- max(tab_contr$contribution) + 0.15
 
-  # returning results
+      # Add means and counts to the plot (same level)
+      for (i in 1:length(unique(tab_contr$predictor))) {
+        text(i, text_y, paste("Mean =", round(means[i], 2)), pos = 3)
+        text(i, text_y , paste("N =", counts[i]), pos = 1)
+      }
+    }
+
+
+  } else {
+    tab_contr <- var_importance_ind(x)
+
+    if (plot){
+      # Create a barplot
+      barplot(tab_contr$contribution, names = tab_contr$predictor, horiz = FALSE)
+    }
+  }
+
   return(tab_contr)
+
 }
 
 

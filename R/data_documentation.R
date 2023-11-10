c1158ecfdf5867ddb18ad0e8b84fe7e0905ecf7e
marlonecobos <manubio13@gmail.com>
2023-11-10 16:30:33 -0600

    v0.1.4 data for model fitting added and documented

---
+++ R/data_documentation.R
@@ -0,0 +1,11 @@
+#' Example of results obtained from GLM calibration using enmpa
+#'
+#' A list of results from GLM calibration.
+#'
+#' @format A list with results from the function `\link{calibration_glm}`.
+#'
+#' @examples
+#' data("cal_res", package = "enmpa")
+#'
+#' str(cal_res)
+"cal_res"
